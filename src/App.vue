279678d310e93a6f556f1f887fbe82b47f88254e
Tyler Morales <moratyle@gmail.com>
2021-01-03 17:15:29 -0600

    💄 Add base styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+  <div id="app"></div>
 </template>
 
 <script>
@@ -9,4 +9,7 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+#app {
+}
+</style>
