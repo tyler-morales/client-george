266ebe6c6484337a9fc88ec63595b9b8ae067b90
Tyler Morales <moratyle@gmail.com>
2021-01-06 14:08:15 -0600

    🗃  Add data

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
     <NavBar />
-    <div class="main-ctnr"></div>
+    <Hero :hero="hero" />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
+import Hero from '@/components/Hero'
 
 export default {
   name: 'App',
-  components: { NavBar },
+  components: {
+    NavBar,
+    Hero,
+  },
+  data() {
+    return {
+      hero: {
+        title: 'Chief Tax Judge, MN, Rte.s',
+        name: 'George Perez',
+        description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat.`,
+        altTag: 'Image of George Perez at a Courtroom',
+        image: require('../src/assets/hero.jpg'),
+        // webp: require('../assets/images/iss_tracker/thumbnail.webp'),
+      },
+    }
+  },
 }
 </script>
 
